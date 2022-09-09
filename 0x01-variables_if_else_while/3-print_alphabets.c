2e76f13924dc8b287c31dcc731f9313afdcbd586
OdogwuNwoke <kingice3453@gmail.com>
2022-09-09 12:03:47 -0700

    Print the alphabets in lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+	int i;
+
+	for (i = 0; i < 26; ++i)
+	{
+		putchar(alpha);
+		alpha += 1;
+	}
+
+	char alpha1 = 'A';
+	int j;
+
+	for (j = 0; j < 26; ++j)
+	{
+		putchar(alpha1);
+		alpha1 += 1;
+	}
+	putchar('\n');
+	return (0);
+}
+	
