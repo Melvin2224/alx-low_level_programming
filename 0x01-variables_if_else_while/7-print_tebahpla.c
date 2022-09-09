1556873759833cc3956bc890585bb64fc0bfb505
OdogwuNwoke <kingice3453@gmail.com>
2022-09-09 13:03:43 -0700

    print the alphabets in reverse, in lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char alpha = 'z';
+	int i;
+
+	for (i = 0; i < 26; ++i)
+	{
+		putchar(alpha);
+		alpha -= 1;
+	}
+	putchar('\n');
+	return (0);
+}
