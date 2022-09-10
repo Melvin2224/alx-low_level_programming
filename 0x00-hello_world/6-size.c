509eb60b3066a2c9789a507b36bee1c370993674
OdogwuNwoke <kingice3453@gmail.com>
2022-09-10 07:29:11 -0700

    Display the sizes of C data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+
+
 /**
  * main -Entry point
  *
  * Return: Always 0 (success)
  */
-int main(void) 
+int main(void)
 {
 
-	printf("Size of char: %li byte(s)", sizeof(char));                                                                               
-	printf("\nSize of int: %li byte(s)", sizeof(int));                                                                               
-	printf("\nSize of long int: %li byte(s)", sizeof(unsigned int));                                                                 
+	printf("Size of char: %li byte(s)", sizeof(char));
+	printf("\nSize of int: %li byte(s)", sizeof(int));
+	printf("\nSize of long int: %li byte(s)", sizeof(unsigned int));
 	printf("\nSize of long long int: %li byte(s)", sizeof(long int));
 	printf("\nSize of float: %ld byte(s)\n", sizeof(float));
 	return (0);
