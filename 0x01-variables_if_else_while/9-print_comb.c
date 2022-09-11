c2a4876a279ef99e4a79435d8de2679f32adde81
OdogwuNwoke <kingice3453@gmail.com>
2022-09-11 12:43:36 -0700

    Display numbers 0-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/**
+ * main - This is where the program starts executing from
+ *
+ * Return: This always returns 0 to show that the proghram ran sucessfully
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		
+	}
+	putchar('\n');
+	return (0);
+}
+
