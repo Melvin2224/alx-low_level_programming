730e85b8b20675165c4c20d190629c466873f908
OdogwuNwoke <kingice3453@gmail.com>
2022-09-11 12:31:28 -0700

    Display c data type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int integer;
 	char character;
-	long int aunsigned;
+	long int along_int;
 	long long int along;
 	float afloat;
 
 	printf("Size of a char: %zu byte(s)", sizeof(character));
 	printf("\nSize of an int: %zu byte(s)", sizeof(integer));
-	printf("\nSize of a long int: %zu byte(s)", sizeof(aunsigned));
+	printf("\nSize of a long int: %zu byte(s)", sizeof(along_int));
 	printf("\nSize of a long long int: %zu byte(s)", sizeof(along));
 	printf("\nSize of a float: %zu byte(s)\n", sizeof(afloat));
 	return (0);
