0c188e3ecf70df1cb725019d4ebc9e1ffc2b9c6d
OdogwuNwoke <kingice3453@gmail.com>
2022-09-13 09:11:54 -0700

    Display alphabets a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+/**
+ * print_alphabet - will display alphabet a-z in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	for (alpha = 'a'; alpha < 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+}
+
+
+/**
+ * main - Entry point for the code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
