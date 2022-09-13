d7c7fb2f6e0d1d18f46262dcc2ca99921de8e212
OdogwuNwoke <kingice3453@gmail.com>
2022-09-13 10:04:19 -0700

    Display the alphabets a-z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -17,15 +17,3 @@ void print_alphabet(void)
 	}
 	putchar('\n');
 }
-
-
-/**
- * main - Entry point for the code
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
