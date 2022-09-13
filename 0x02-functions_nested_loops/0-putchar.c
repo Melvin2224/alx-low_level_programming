4a5f9a5f3efec8baa31a2d03ca642679068c0da8
OdogwuNwoke <kingice3453@gmail.com>
2022-09-13 16:19:53 -0700

    Display _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include "main.h"
 
 
+int main(void)
+{
+	print_putchar();
+	return (0);
+}
 /**
- * main - Prints _putchar
+ * print_putchar - Prints _putchar
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_putchar(void)
 {
-	
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
 void print_alphabet(void);
+void print_putchar(void);
