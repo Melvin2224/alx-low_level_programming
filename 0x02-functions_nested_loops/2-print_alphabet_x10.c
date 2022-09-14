95f19e86291b5fc94c9238d81823abb7924e38ba
OdogwuNwoke <kingice3453@gmail.com>
2022-09-13 17:13:14 -0700

    Display a-z in 10 lines

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * main - print output of print_alphabet_x10()
- *
- * Return: Always 0;
- */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
+
 /**
  * print_alphabet_x10 - this function will display the alphabets a-z throug 10 lines
  *
