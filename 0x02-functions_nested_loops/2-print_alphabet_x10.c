0d68d92bdb1a4b15933385d9ed86f264a26ee74a
OdogwuNwoke <kingice3453@gmail.com>
2022-09-13 17:15:23 -0700

    Display alphabet a-z to span through 10 lines

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,15 @@
 
 
 /**
- * print_alphabet_x10 - this function will display the alphabets a-z throug 10 lines
+ * print_alphabet_x10 - this function will display
+ * the alphabets a-z throug 10 lines
  *
  * Return: Always 0
  */
 void print_alphabet_x10(void)
 {
 	int i = 0, j;
+
 	while (i < 10)
 	{
 		for (j = 'a'; j <= 'z'; j++)
