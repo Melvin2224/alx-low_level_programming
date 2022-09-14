e8beb1db4cf95d1bf7b749bf3dcd58d7a09eade7
OdogwuNwoke <kingice3453@gmail.com>
2022-09-14 09:00:54 -0700

    Display absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * _abs - this function, returns the non-negative
+ * value of a number ie the absolute value
+ * @num - First operrand
+ *
+ * Return: depends on condition
+ */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return -num;
+	}
+	else
+	{
+		return num;
+	}
+	return (0);
+}
