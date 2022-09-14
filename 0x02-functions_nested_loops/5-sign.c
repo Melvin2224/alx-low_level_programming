02d98dcbf27fab76d7b8a58a882c7cc88e7b2951
OdogwuNwoke <kingice3453@gmail.com>
2022-09-14 06:19:36 -0700

    Return the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_sign - This function displays the sign of a number
+ * if the number is greater than 0, it displays +, if it is zero,
+ * it displays 0 and if it is less than 0, it displays -.
+ *
+ * Return: What it return depends on ie if number is greater than
+ * zero, it returns 1, if it is equal to zero, it returns 0, if
+ * it is less than zero, it return -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+}
