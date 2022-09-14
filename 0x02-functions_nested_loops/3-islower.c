bf20eb03ee647dd5e7d8896f95fdc7ed5edfa779
OdogwuNwoke <kingice3453@gmail.com>
2022-09-13 17:27:51 -0700

    Check if variable value is in lower/upper case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * _islower() - This function will check if a variable 
+ * is in lower case or upper case
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return(0)
+	}
+	return (0);
+}
