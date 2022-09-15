fca386358d500e25287fdd68e6f42c4b84e131aa
OdogwuNwoke <kingice3453@gmail.com>
2022-09-15 16:36:24 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * more_numbers - This function prints the numbers 0-14 10 times
+ *
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int i, j;
+	
+	i = 0;
+	while (i < 10)
+	{
+		for(j = 0; j < 15; j++)
+		{
+			_
