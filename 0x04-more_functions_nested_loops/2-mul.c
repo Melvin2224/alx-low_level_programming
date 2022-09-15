a11f1d2f37ee551b8c9f3597477cf48dd68f4dca
OdogwuNwoke <kingice3453@gmail.com>
2022-09-15 03:28:44 -0700

    Finds the multiplication of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - This function multiplies two integers
+ * @a: first operrand
+ * @b: second operrand
+ *
+ * Return: Always 0 if siuccessful
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
