ad77b452003cce2b2e7ff93689649e27b6b57bff
OdogwuNwoke <kingice3453@gmail.com>
2022-09-15 03:06:03 -0700

    Checks if variable value is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - This function, checks if a varaible value is in upper case.
+ * @c: first parameter
+ *
+ * Return: returns 1 if c is uppercase and 0 if not
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
