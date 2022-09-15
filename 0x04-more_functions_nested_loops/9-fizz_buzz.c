3f7eb692f2e2e65221ee8ad92fb908860bb6bb4a
OdogwuNwoke <kingice3453@gmail.com>
2022-09-15 06:26:32 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+/**
+ * main - Fizbuzz
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char first[] = "Fizz";
+	char third[] = "FizzBuzz";
+	char second[] = "Buzz";
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s ", third);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s ", first);
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s ", second);
+		}
+		else
+		{
+			printf("%i ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
