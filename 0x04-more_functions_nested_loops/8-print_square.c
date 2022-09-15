de5928b37df5cdb869f7ebad5446e0ca12bd2cc4
OdogwuNwoke <kingice3453@gmail.com>
2022-09-15 16:13:46 -0700

    Print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * print_square - This funtion prints a square made up of #
+ * print_square - this function prints a square using #
  * @size: determines the size of the square
  *
- * Return: Always 0
+ * Return: 0 Always
  */
 void print_square(int size)
 {
-	int i;
-	int j;
+	int i, j;
 
 	if (size > 0)
 	{
-		for (i = 1; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
 			for (j = 1; j <= size; j++)
 			{
@@ -21,9 +20,9 @@ void print_square(int size)
 			}
 			_putchar('\n');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
-	else if (size == 0 || size < 0)
+	else if (i == 0 || i < 0)
 	{
 		_putchar('\n');
 	}
