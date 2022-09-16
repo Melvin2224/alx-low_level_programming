3c800bf29d58b8ae3f848af492ef60aee2d69ec9
OdogwuNwoke <kingice3453@gmail.com>
2022-09-16 04:32:55 -0700

    modify main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,17 +3,15 @@
 
 /**
  * _isupper - function to check for uppercase
- * @c: the character to be checked
- *
+ * @c: The charactr to be checked
  * Return: return type is int
  * File: main.h
- * Description: custom function prototype declration
+ * Description: Custom function prototype declarations
  */
 
-
-int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int _putchar(char c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
@@ -21,7 +19,16 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_num_buzz(void);
 void print_triangle(int size);
 void print_number(int n);
-
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void _islower(int c);
+void _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void print_to_98(int n);
 #endif
