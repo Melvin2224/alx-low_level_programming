72ea2475fff46a50c4184ea3b1d02a444cb22399
OdogwuNwoke <kingice3453@gmail.com>
2022-09-20 05:48:38 -0700

    Convert the value stored in a memory address

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 - This functions points to a variable memory address
+ * using its pointer variable parameter and chnages the value stored
+ * in the memory address to 98
+ * @n: POinter variable
+ * Return: it has no return value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
