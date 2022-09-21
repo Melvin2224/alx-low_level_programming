bc13be7e5a05de28933415905b8e6c8da73ab77a
OdogwuNwoke <kingice3453@gmail.com>
2022-09-21 04:27:12 -0700

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * print_rev - This function takes a string argument and reverses the string
+ * @s: pointer that points to the string;
+ *
+ * Return: No return value
+ */
+void print_rev(char *s)
+{
+	int length = strlen(s);
+	int i;
+
+	for (i = length; i >= 0; --i)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
