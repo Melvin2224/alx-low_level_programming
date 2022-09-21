c03ec27904d0ffe6d77515a98457e5ca470be178
OdogwuNwoke <kingice3453@gmail.com>
2022-09-21 09:54:07 -0700

    Copy string into buffer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * _strcpy - Copies a string to another string
+ * @dest: first pointer
+ * @src: second pointer
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
