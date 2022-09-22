60162019249ab029ac84e3970c23af44516572cd
OdogwuNwoke <kingice3453@gmail.com>
2022-09-21 18:25:41 -0700

    Concatenate n numbers of second string to first string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+ * _strncat - Acceps two string arguments and concatenantes from the value
+ * of the second one up to n to the first one
+ * @dest: first pointer pointing to first string
+ * @src: second pointer pointing to second string
+ * @n: Determines how much of second string will be concatenated with first
+ * string
+ *
+ * Return: pointer dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = strlen(dest);
+	int src_len = strlen(src);
+	int i;
+
+	for (i = 0; i < n && i <= str_len; ++i)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+}
