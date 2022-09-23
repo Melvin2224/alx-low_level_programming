13e7dd73a277cc4f664a01928b62d6c397126a55
OdogwuNwoke <kingice3453@gmail.com>
2022-09-23 15:53:19 -0700

    Compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <string.h>
-#include <stdio.h>
 
 
 /**
@@ -14,14 +13,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int s1_len = strlen(s1);
-	int s2_len = strlen(s2);
+	int i;
 
-	if (s1 > s2)
-		return (15);
-	else if (s1 < s2)
-		return (-15);
-	else if (s1 == s2)
-		return (0);
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
 	return (0);
 }
