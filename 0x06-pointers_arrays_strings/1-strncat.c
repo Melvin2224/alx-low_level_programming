352f64666a7768b21a58a146721a23c186d061e4
OdogwuNwoke <kingice3453@gmail.com>
2022-09-23 07:37:30 -0700

    Concatenate specific string characters

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,10 +16,10 @@ char *_strncat(char *dest, char *src, int n)
 	int dest_len = strlen(dest);
 	int i;
 
-	for (i = 0; i <= n && src[i] != '\0'; ++i)
+	for (i = 0; i < n && src[i] != '\0'; ++i)
 	{
-		dest[dest_len + 1] = src[i];
-		dest_len += 1;
+		dest[dest_len + i] = src[i];
 	}
+	dest[dest_len + i] = '\0';
 	return (dest);
 }
