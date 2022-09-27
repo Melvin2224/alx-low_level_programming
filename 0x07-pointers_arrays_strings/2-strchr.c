8174d4e4b582bb42d19804b6abc62214a3d5de10
OdogwuNwoke <kingice3453@gmail.com>
2022-09-27 16:01:37 -0700

    Search for a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+
+/**
+ * _strchr - This function checks for a specific character
+ * in a string
+ * @s: pointer to string to be checked
+ * @c: char to be check for
+ * Return: returns the memory address of string to be checked
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		return (&s[i]);
+	}
+	return (0);
+}
