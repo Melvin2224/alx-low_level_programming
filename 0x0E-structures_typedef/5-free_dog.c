aa860cfbf9baa47098b70b6d584b8a5cabcc29e9
OdogwuNwoke <kingice3453@gmail.com>
2022-10-11 21:34:27 -0700

    Free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dog
+ * @d: pointer to a dog of type dog_t
+ *
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
