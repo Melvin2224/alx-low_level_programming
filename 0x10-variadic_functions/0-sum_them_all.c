95b58dddf65baf38d2ae2d7c3d2ef2af6bfe7465
OdogwuNwoke <kingice3453@gmail.com>
2022-10-14 21:11:05 -0700

    Sum variable arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,11 +11,11 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum;
+	va_list ap;
 
 	sum = 0;
 	if (n == 0)
 		return (0);
-	va_list ap;
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
